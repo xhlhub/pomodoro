1bdaa7d974e7b9320e9b30ce7a198db6ee59c5c3
xie hailong <xie.h@ctw.inc>
2025-07-21 11:16:41 +0800

    clock icon

--- src/components/ClockIcon.js
+++ src/components/ClockIcon.js
@@ -22,22 +22,17 @@ function ClockIcon({ size = 20, color = 'white' }) {
       />
       
       {/* 时钟刻度 */}
-      <g stroke={color} strokeWidth="1">
-        {/* 12点钟位置 */}
+      {/* <g stroke={color} strokeWidth="1">
         <line x1="12" y1="3" x2="12" y2="5" strokeWidth="2"/>
-        {/* 3点钟位置 */}
         <line x1="21" y1="12" x2="19" y2="12" strokeWidth="2"/>
-        {/* 6点钟位置 */}
         <line x1="12" y1="21" x2="12" y2="19" strokeWidth="2"/>
-        {/* 9点钟位置 */}
         <line x1="3" y1="12" x2="5" y2="12" strokeWidth="2"/>
         
-        {/* 其他小刻度 */}
         <line x1="18.4" y1="5.6" x2="17.7" y2="6.3"/>
         <line x1="18.4" y1="18.4" x2="17.7" y2="17.7"/>
         <line x1="5.6" y1="5.6" x2="6.3" y2="6.3"/>
         <line x1="5.6" y1="18.4" x2="6.3" y2="17.7"/>
-      </g>
+      </g> */}
       
       {/* 时针 - 指向2点钟方向 */}
       <line 
